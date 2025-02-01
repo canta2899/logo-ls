d132fcf35ea6934e609d5662fca77737986b9fd0
canta2899 <andreacantarutti@icloud.com>
2025-02-01 22:00:05 +0100

    feat: support for sticky bit on file permissions

--- format/format_unix.go
+++ format/format_unix.go
@@ -42,13 +42,25 @@ func GetHardLinkCount(absPath string) uint64 {
 }
 
 func GetModeExtended(fi *os.FileInfo, fullPath string) string {
-
 	mode := (*fi).Mode()
 	modeStr := mode.String()
 
-	// Check of extended attributes
-	count, err := unix.Listxattr(fullPath, nil)
+	// Get raw stat info
+	var stat unix.Stat_t
+	if err := unix.Stat(fullPath, &stat); err == nil {
+		// Check if the sticky bit is set
+		if stat.Mode&unix.S_ISVTX != 0 {
+			// Check if others have execute permission (t) or not (T)
+			if stat.Mode&unix.S_IXOTH != 0 {
+				modeStr = modeStr[:9] + "t" // "t" (executable by others)
+			} else {
+				modeStr = modeStr[:9] + "T" // "T" (not executable by others)
+			}
+		}
+	}
 
+	// Check for extended attributes (@)
+	count, err := unix.Listxattr(fullPath, nil)
 	if err == nil && count > 0 {
 		modeStr += "@"
 	}
